f144df17ff1501895ec2d63a5c07016f8ab908f7
GET Academy <terje@kolderup.net>
2021-05-28 11:32:31 +0200

    Initial commit

---
+++ InheritanceInWPF/ButtonAndLabelPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace InheritanceInWPF
+{
+    class ButtonAndLabelPanel : StackPanel
+    {
+        private int _clicks = 0;
+        private Label _label;
+
+        public ButtonAndLabelPanel()
+        {
+            var button = new Button { Content = "Hei" };
+            Children.Add(button);
+            button.Click += ButtonClick;
+            _label = new Label() { Content = "Hei" };
+            Children.Add(_label);
+        }
+
+        private void ButtonClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _clicks++;
+            _label.Content = _clicks;
+        }
+    }
+}
---
+++ InheritanceInWPF/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InheritanceInWPF
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var application = new Application();
+            var window = new Window();
+            var panel1 = new ButtonAndLabelPanel();
+            var panel2 = new ButtonAndLabelPanel();
+            var panel3 = new ButtonAndLabelPanel();
+
+            var panel = new StackPanel();
+            panel.Children.Add(panel1);
+            panel.Children.Add(panel2);
+            panel.Children.Add(panel3);
+            window.Content = panel;
+            application.Run(window);
+        }
+    }
+}
